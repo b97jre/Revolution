bc6adf8417785434e0407b664de0eb634d230889
Johan Reimegård <johan.reimegard@gmail.com>
2013-09-03 14:11:37 +0200

    minor bug

--- ASEcalculations.R
+++ ASEcalculations.R
@@ -8,14 +8,15 @@
 main <-function(fileName="BWA_genome.raw.all_FREEC50k.repeatRegions.heterozygousRegions.Crubella_183_only_exons_unique.vcf.Sample.vcf.Rfriendly", dataDir="/gulo/proj_nobackup/b2012122/private/ASE",
                 annotation="BWA_genome.raw.all_FREEC50k.repeatRegions.heterozygousRegions.Crubella_183_only_exons_unique.vcf.Sample.Crubella_183_gene.VCFannotaion",  
                 rounds = 10 ,cutoffNominal=0.005, cutoffAdjusted=0.1){
-             
   
-    file <- paste(dataDir,fileName,sep="/")
   
-    #load dataset  
-    sampleData <- read.table(file,header=TRUE,sep ="\t")
-    annotationData <- read.table(annotationFile,header=FALSE,sep ="\t")
-    sampleData$annotation=annotationData$V3
+  file <- paste(dataDir,fileName,sep="/")
+  annotationFile <- paste(dataDir,annotation,sep="/")
+  
+  #load dataset  
+  sampleData <- read.table(file,header=TRUE,sep ="\t")
+  annotationData <- read.table(annotationFile,header=FALSE,sep ="\t")
+  sampleData$annotation=annotationData$V3
   
   # read in different samples
   DNAInterSamples <- c("Inter3.1","Inter4.1","Inter5.1")
@@ -24,42 +25,43 @@ main <-function(fileName="BWA_genome.raw.all_FREEC50k.repeatRegions.heterozygous
   RNAIntraSamples <- c("Intra6_3_F","Intra6_3_L","Intra7_2_1_F","Intra7_2_1_L","Intra7_2_2_F","Intra7_2_2_L","Intra7_2_3_F","Intra7_2_3_L","Intra8_2_1_F","Intra8_2_1_L")
   AllSamples <- c(DNAInterSamples,DNAIntraSamples,RNAInterSamples,RNAIntraSamples)
   
-    
+  
   # 
   ASEinfo = data.frame()
   Inter3_1_DNA <- c("Inter3.1")
   Inter3_1_RNA <-c("Inter3_1_1_F","Inter3_1_1_L")  
   Inter3_1_SampleData <- getPvalues(sampleData,Inter3_1_DNA,Inter3_1_RNA)
   ASEinfo <- rbind(ASEinfo, getASEinfo(Inter3_1_SampleData,Inter3_1_RNA,rounds,cutoffNominal,cutoffAdjusted))
-    
+  
   Inter4_1_DNA <- c("Inter4.1")
   Inter4_1_RNA <-c("Inter4_1_1_F","Inter4_1_1_L","Inter4_1_2_F","Inter4_1_2_L","Inter4_1_3_F","Inter4_1_4_L")  
   Inter4_1_SampleData <- getPvalues(sampleData,Inter4_1_DNA,Inter4_1_RNA)
   ASEinfo <- rbind(ASEinfo, getASEinfo(Inter4_1_SampleData,Inter4_1_RNA,rounds,cutoffNominal,cutoffAdjusted))
-    
+  
   Inter5_1_DNA <- c("Inter5.1")
   Inter5_1_RNA <-c("Inter5_1_1_F","Inter5_1_1_L")  
   Inter5_1_SampleData <- getPvalues(sampleData,Inter5_1_DNA,Inter5_1_RNA)
   ASEinfo <- rbind(ASEinfo, getASEinfo(Inter5_1_SampleData,Inter5_1_RNA,rounds,cutoffNominal,cutoffAdjusted))
-    
+  
   Intra6_3_DNA <- c("Intra6.3")
   Intra6_3_RNA <-c("Intra6_3_F","Intra6_3_L")
   Intra6_3_SampleData <- getPvalues(sampleData,Intra6_3_DNA,Intra6_3_RNA)
   ASEinfo <- rbind(ASEinfo, getASEinfo(Intra6_3_SampleData,Intra6_3_RNA,rounds,cutoffNominal,cutoffAdjusted))
-    
+  
   Intra7_2_DNA <- c("Intra7.2")
   Intra7_2_RNA <-c("Intra7_2_1_F","Intra7_2_1_L","Intra7_2_2_F","Intra7_2_2_L","Intra7_2_3_F","Intra7_2_3_L")
   Intra7_2_SampleData <- getPvalues(sampleData,Intra7_2_DNA,Intra7_2_RNA)
   ASEinfo <- rbind(ASEinfo, getASEinfo(Intra7_2_SampleData,Intra7_2_RNA,rounds,cutoffNominal,cutoffAdjusted))
-    
+  
   Intra8_2_DNA <- c("Intra8.2")
   Intra8_2_RNA <-c("Intra8_2_1_F","Intra8_2_1_L")
   Intra8_2_SampleData <- getPvalues(sampleData,Intra8_2_DNA,Intra8_2_RNA)
   ASEinfo <- rbind(ASEinfo, getASEinfo(Intra8_2_SampleData,Intra8_2_RNA,rounds,cutoffNominal,cutoffAdjusted))
-    
-	#Under development
-	printPvalues(DataSetInterHeteroAbundantBOTHwithPvalue,RNAInterSamples,DNAInterSamples,"test.pdf")	
-	
+  
+  #Under development
+  #printPvalues(DataSetInterHeteroAbundantBOTHwithPvalue,RNAInterSamples,DNAInterSamples,"test.pdf")	
+  return (ASEinfo)
+  
 }
 
 
@@ -79,7 +81,7 @@ getPvalues <- function(sampleData,DNAsamples,RNAsamples, RNAlow=40, RNAhigh=2000
   # Correct for multiple testing
   DataSetFilteredAbundantBOTHwithPvalue<-as.data.frame(DataSetFilteredAbundantBOTHwithPvalue)
   DataSetInterHeteroAbundantBOTHwithPvalueCorrected <- MultipleTestCorrection(DataSetFilteredAbundantBOTHwithPvalue,RNAsamples)
-
+  
   
   return(DataSetInterHeteroAbundantBOTHwithPvalueCorrected)
   
@@ -90,84 +92,84 @@ getPvalues <- function(sampleData,DNAsamples,RNAsamples, RNAlow=40, RNAhigh=2000
 
 
 printPvalues <- function(Dataset,RNAsamples,DNAsamples, pdfFileName){
-		
-	pdf(pdfFileName)
-	par(mfrow=c(length(RNAsamples),2), bty="l", cex=0.6)
-	for(i in 1:length(RNAsamples)){
-		for(j in 1:length(DNAsamples[j])){
-			RNAsample = strsplit(RNAsamples[i], '_')
-			DNAsample = strsplit(DNAsamples[j], '\\.')
-			if( RNAsample[[1]][1]==DNAsample[[1]][1]){
-				
-					name = paste(RNAsamples[i], "pValue", sep="_")
-					hist(Dataset[name],breaks=40)		
-					name = paste(RNAsamples[i], "fraction", sep="_")
-					name2 = paste(DNAsamples[j], "fraction", sep="_")
-					boxScatterPlot(Dataset[name],Dataset[name],ylabText="DNA distribution",xlabText="RNA distribtuion",title = paste( "ASE distribution"))	
-			}
-		}
-	}
-	dev.off()
-	
+  
+  pdf(pdfFileName)
+  par(mfrow=c(length(RNAsamples),2), bty="l", cex=0.6)
+  for(i in 1:length(RNAsamples)){
+    for(j in 1:length(DNAsamples[j])){
+      RNAsample = strsplit(RNAsamples[i], '_')
+      DNAsample = strsplit(DNAsamples[j], '\\.')
+      if( RNAsample[[1]][1]==DNAsample[[1]][1]){
+        
+        name = paste(RNAsamples[i], "pValue", sep="_")
+        hist(Dataset[name],breaks=40)		
+        name = paste(RNAsamples[i], "fraction", sep="_")
+        name2 = paste(DNAsamples[j], "fraction", sep="_")
+        boxScatterPlot(Dataset[name],Dataset[name],ylabText="DNA distribution",xlabText="RNA distribtuion",title = paste( "ASE distribution"))	
+      }
+    }
+  }
+  dev.off()
+  
 }
 
 
 boxScatterPlot <- function(x,y,ylabText="Major readCount",xlabText="Minor readCount",title ="Enhanced Scatterplot"){
-	par(fig=c(0,0.8,0,0.8), new=TRUE)
-	plot(x, y , xlab=xlabText,
-			ylab=ylabText)
-	par(fig=c(0,0.8,0.55,1), new=TRUE)
-	boxplot(x, horizontal=TRUE, axes=FALSE)
-	par(fig=c(0.65,1,0,0.8),new=TRUE)
-	boxplot(y, axes=FALSE)
-	mtext(title, side=3, outer=TRUE, line=-3)
-	
+  par(fig=c(0,0.8,0,0.8), new=TRUE)
+  plot(x, y , xlab=xlabText,
+       ylab=ylabText)
+  par(fig=c(0,0.8,0.55,1), new=TRUE)
+  boxplot(x, horizontal=TRUE, axes=FALSE)
+  par(fig=c(0.65,1,0,0.8),new=TRUE)
+  boxplot(y, axes=FALSE)
+  mtext(title, side=3, outer=TRUE, line=-3)
+  
 }
 
 
 
 
 filterCallData  <- function(testData , samples, call="0/1"){
-	
-	# filter for heterozygous sites in all samples
-	for(i in 1:length(samples)){
-		name = paste(samples[i], "Call", sep="_")
-		testData <- testData[testData[name]==call,]		
-	}
-	return (testData)
+  
+  # filter for heterozygous sites in all samples
+  for(i in 1:length(samples)){
+    name = paste(samples[i], "Call", sep="_")
+    testData <- testData[testData[name]==call,]		
+  }
+  return (testData)
 }
 
 filterTotalData <- function(Dataset , samples,cutoff=c(40,200)){
-	for(i in 1:length(samples)){
-		name = paste(samples[i], "Total", sep="_")
-		Dataset <- Dataset[Dataset[name]>cutoff[1]	,]		
-	}
-	return (Dataset)
+  for(i in 1:length(samples)){
+    name = paste(samples[i], "Total", sep="_")
+    Dataset <- Dataset[Dataset[name]>cutoff[1]	,]		
+  }
+  return (Dataset)
 }
 
 
 Pvaluescalculation <- function(testData,RNAsamples,DNAsamples){
-	for(i in 1:length(RNAsamples)){
-		for(j in 1:length(DNAsamples)){
-			RNAsample = strsplit(RNAsamples[i], '_')
-			DNAsample = strsplit(DNAsamples[j], '\\.')
-			if( RNAsample[[1]][1]==DNAsample[[1]][1]){
-				print(paste(DNAsamples[j], RNAsamples[i], sep = " comparing "))
-				count1RNA=paste(RNAsamples[i], "Count1", sep="_")
-				TotalRNA=paste(RNAsamples[i], "Total", sep="_")
-				count1DNA=paste(DNAsamples[j], "Count1", sep="_")
-				TotalDNA=paste(DNAsamples[j], "Total", sep="_")
-				pValuesName= paste(RNAsamples[i], "pValue", sep="_")
-				#print(testData[count1RNA]/testData[TotalRNA])
-				#print(paste(testData[count1DNA][1], testData[TotalDNA][1], sep = " of total "))
-				
-				
-				testData[pValuesName]<- apply(testData,1, BinTest,  count1RNA, TotalRNA, count1DNA, TotalDNA)
-			}
-		}
-	}
-	return (testData) 
-	
+  for(i in 1:length(RNAsamples)){
+    for(j in 1:length(DNAsamples)){
+      RNAsample = strsplit(RNAsamples[i], '_')
+      DNAsample = strsplit(DNAsamples[j], '\\.')
+      if( RNAsample[[1]][1]==DNAsample[[1]][1]){
+        print(paste(DNAsamples[j], RNAsamples[i], sep = " comparing "))
+        count1RNA=paste(RNAsamples[i], "Count1", sep="_")
+        TotalRNA=paste(RNAsamples[i], "Total", sep="_")
+        count1DNA=paste(DNAsamples[j], "Count1", sep="_")
+        TotalDNA=paste(DNAsamples[j], "Total", sep="_")
+        pValuesName= paste(RNAsamples[i], "pValue", sep="_")
+        #print(testData[count1RNA]/testData[TotalRNA])
+        #print(paste(testData[count1DNA][1], testData[TotalDNA][1], sep = " of total "))
+        
+        
+        testData[pValuesName]<- apply(testData,1, BinTest,  count1RNA, TotalRNA, count1DNA, TotalDNA)
+      }
+    }
+  }
+  return (testData) 
+  
 }
 
 
@@ -187,8 +189,8 @@ MultipleTestCorrection <- function(testData,RNAsamples) {
 
 
 BinTest <- function(x, count1RNA, TotalRNA,count1DNA,TotalDNA){
-	temp <- binom.test(as.numeric(x[count1RNA]),as.numeric(x[TotalRNA]),p=(as.numeric(x[count1DNA])/as.numeric(x[TotalDNA])), alternative="two.sided")
-	return (temp$p.value)
+  temp <- binom.test(as.numeric(x[count1RNA]),as.numeric(x[TotalRNA]),p=(as.numeric(x[count1DNA])/as.numeric(x[TotalDNA])), alternative="two.sided")
+  return (temp$p.value)
 }
 
 
@@ -210,22 +212,18 @@ getASEinformation <- function(DatasetOneSNPperGene,RNAsamples,cutoffNominal=0.00
     nrOfGenes <- length (Data)
     Nominal <- Data[which(Data<cutoffNominal)]
     nrOfASEnominal <- length (Nominal)
-  
-  
+    
+    
     columnName  <- paste(RNAsamples[i],"pValueCorrected",sep="_")
     Data <- DatasetOneSNPperGene[,columnName]
     Adjusted <- Data[which(Data<cutoffAdjusted)]
     nrOfASEAdjusted <- length (Adjusted)
-  
+    
     rowinfo <- data.frame("Name"=RNAsamples[i],"NrOfGenes"=nrOfGenes,"NominalpValue"=cutoffNominal,"Nominal_Significant"=nrOfASEnominal,"Adjusted_Pvalue"=cutoffAdjusted,"Adjusted_count"=nrOfASEAdjusted)
     info <- rbind(info,rowinfo)
     
   }
   return (info)
-}
-
-getASEinformationAdjusted(Dataset,RNAsample,cutoff){
-  Inter3_1_1_F_pValueCorrected
 }
 
 getOneSNPperGene <- function(Dataset,classifier="annotation", cutoff = 0.05){
@@ -235,25 +233,25 @@ getOneSNPperGene <- function(Dataset,classifier="annotation", cutoff = 0.05){
 }
 
 duplicated.random <- function(x, incomparables = FALSE, ...){
-    if ( is.vector(x) )
-    {
-      permutation = sample(length(x))
-      x.perm      = x[permutation]
-      result.perm = duplicated(x.perm, incomparables, ...)
-      result      = result.perm[order(permutation)]
-      return(result)
-    }
-    else if ( is.matrix(x) )
-    {
-      permutation = sample(nrow(x))
-      x.perm      = x[permutation,]
-      result.perm = duplicated(x.perm, incomparables, ...)
-      result      = result.perm[order(permutation)]
-      return(result)
-    }
-    else
-    {
-      stop(paste("duplicated.random() only supports vectors",
-                 "matrices for now."))
-    }
+  if ( is.vector(x) )
+  {
+    permutation = sample(length(x))
+    x.perm      = x[permutation]
+    result.perm = duplicated(x.perm, incomparables, ...)
+    result      = result.perm[order(permutation)]
+    return(result)
   }
+  else if ( is.matrix(x) )
+  {
+    permutation = sample(nrow(x))
+    x.perm      = x[permutation,]
+    result.perm = duplicated(x.perm, incomparables, ...)
+    result      = result.perm[order(permutation)]
+    return(result)
+  }
+  else
+  {
+    stop(paste("duplicated.random() only supports vectors",
+               "matrices for now."))
+  }
+}
